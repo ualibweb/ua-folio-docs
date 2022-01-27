f022b1357d9d930f13a0262996dc35fc2312646d
Noah Overcash <ncovercash@ua.edu>
2022-01-26 23:21:39 -0600

    Update typings for benchmark schema

--- scripts/api-benchmark/schema/bundle.ts
+++ scripts/api-benchmark/schema/bundle.ts
@@ -1,7 +1,7 @@
 import parser from "@apidevtools/json-schema-ref-parser";
 
 async function run(): Promise<void> {
-  const schema = await parser.dereference(__dirname + "/base.json");
+  const schema = await parser.bundle(__dirname + "/base.json");
   console.log(JSON.stringify(schema));
 }
 
---
+++ scripts/api-benchmark/types.d.ts
@@ -0,0 +1,78 @@
+/**
+ * The main schema for api-benchmark configuration
+ */
+export interface MainConfiguration {
+  configuration: Settings;
+  benchmarks: AnyBenchmark[];
+}
+
+export type AnyBenchmark = BenchmarkPrimary | BenchmarkInternal;
+
+/**
+ * Configuration for an entire api-benchmark run
+ */
+export interface Settings {
+  /**
+   * The list of URLs to test against with any special properties
+   */
+  urls: [TestURL, ...TestURL[]];
+  /**
+   * Where results should be saved
+   */
+  outputFilename: string;
+  proxy?: string | null;
+}
+/**
+ * Configuration for a single URL to test against to run
+ */
+export interface TestURL {
+  base: string;
+  headers?: Record<string, string>;
+}
+/**
+ * Configuration for a benchmark to be ran
+ */
+export interface BenchmarkPrimary {
+  id: string;
+  name?: string;
+  description?: string;
+  init?: string | Endpoint;
+  endpoints: string | Endpoint;
+  teardown?: string | Endpoint;
+  runs: number;
+}
+/**
+ * Configuration for an endpoint to send a request to
+ */
+export interface Endpoint {
+  path: string;
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  queryString?: string | Record<string, string>;
+  requestBody?: null | string;
+  /**
+   * The allowed response code range, rounded to the nearest hundred
+   */
+  requiredResponseCode?:
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | 100
+    | 200
+    | 300
+    | 400
+    | 500;
+}
+/**
+ * Configuration for a benchmark to be used only (not itself ran)
+ */
+export interface BenchmarkInternal {
+  id: string;
+  name?: string;
+  description?: string;
+  init?: string | Endpoint;
+  endpoints: string | Endpoint;
+  teardown?: string | Endpoint;
+  internal: true;
+}
