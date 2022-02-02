a909a738105db1e246258fbc518ca033859b82fb
Noah Overcash <ncovercash@ua.edu>
2022-02-01 19:20:46 -0600

    Delete old api-benchmark

--- scripts/api-benchmark/schema/bundle.ts
+++
@@ -1,9 +0,0 @@
-import parser from "@apidevtools/json-schema-ref-parser";
-import path from "path";
-
-async function run(): Promise<void> {
-  const schema = await parser.bundle(path.resolve(__dirname, "base.json"));
-  console.log(JSON.stringify(schema));
-}
-
-run();
